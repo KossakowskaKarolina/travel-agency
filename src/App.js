37f63569dcbcdce9e00a61e4d761de752e12b50e
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2021-12-09 01:16:51 +0100

    Add routing to countries and regions

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
 import Countries from './components/views/Countries/CountriesContainer';
 import Regions from './components/views/Regions/RegionsContainer';
+import Country from './components/views/Country/CountryContainer'
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -43,6 +44,8 @@ class App extends React.Component {
             <Route exact path='/trips' component={Trips} />
             <Route exact path='/countries' component={Countries} />
             <Route exact path='/regions' component={Regions} />
+            <Route exact path='/trips/:id' component={Trips} />
+            <Route exact path='/country/:id' component={Country} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
