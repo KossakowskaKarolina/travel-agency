095347cc7e0998eae3d7c9be1999e5856df68a42
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-01-20 21:43:40 +0100

    Add OrderOptionIcons component

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const OrderOptionIcons = () => (
-  <div>OrderOptionIcons</div>
+import Icon from '../../common/Icon/Icon'
+import { formatPrice } from '../../../utils/formatPrice';
+
+import styles from './OrderOption.module.scss';
+
+const OrderOptionIcons = ({values, setOptionValue, required, currentValue}) => (
+  <div className={styles.icon}>
+    {required ? '' : (
+      <div className={styles.icon} onClick={() => setOptionValue('')}>
+        <Icon name='times-circle' /> None
+      </div>
+    )}
+    {values.map(value => (
+      <div className={value.id === currentValue ? styles.iconActive : styles.icon} key={value.id} onClick={id => setOptionValue(value.id)}>
+        <Icon name={value.icon} /> {value.name} ({formatPrice(value.price)})
+      </div>
+    ))}
+  </div>
 )
 
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  setOptionValue: PropTypes.func,
+  required: PropTypes.bool,
+  currentValue: PropTypes.string,
+}
+
 export default OrderOptionIcons;
+
+
