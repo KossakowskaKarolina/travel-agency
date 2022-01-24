392b4f0d94f0c20526ac159cd4403ff4269fa8a7
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-01-24 23:38:53 +0100

    Add common tests for OrderOption subcomponents and dropdown test

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -21,3 +21,97 @@ describe('Component OrderOption', () => {
     expect(component.find('.title').text()).toEqual(expectedName);
   });
 });
+
+const optionTypes = {
+  dropdown: 'OrderOptionDropdown',
+  icons: 'OrderOptionIcons',
+  checkboxes: 'OrderOptionCheckboxes',
+  number: 'OrderOptionNumber',
+  text: 'OrderOptionText',
+  date: 'OrderOptionDate',
+};
+
+const mockProps = {
+  id: 'abc',
+  name: 'Lorem',
+  values: [
+    {id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0},
+    {id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100},
+  ],
+  required: false,
+  currentValue: 'aaa', // równe values[0].id - symulujemy sytuację, w której opcja ma już jakąś wartość, którą chcemy zmienić na inną
+  price: '50%',
+  limits: {
+    min: 0,
+    max: 6,
+  },
+};
+
+const mockPropsForType = { // zawiera propsy istotne tylko dla konkretnego typu opcji
+  dropdown: {},
+  icons: {},
+  checkboxes: {currentValue: [mockProps.currentValue]},
+  number: {currentValue: 1},
+  text: {},
+  date: {},
+};
+
+const testValue = mockProps.values[1].id;
+//const testValueNumber = 3;
+
+for(let type in optionTypes){
+  describe(`Component OrderOption with type=${type}`, () => {
+    /* test setup */
+    let component;
+    let subcomponent;
+    let renderedSubcomponent;
+    let mockSetOrderOption;
+
+    beforeEach(() => { //renderujemy komponent przed każdym z testów 'it', aby nie używać shallow w każdym z nich
+      mockSetOrderOption = jest.fn(); // mockowanie funkcji (wbudowane w jesta)
+      component = shallow(
+        <OrderOption
+          type={type}
+          setOrderOption={mockSetOrderOption}
+          {...mockProps}
+          {...mockPropsForType[type]}
+        />,
+      );
+      subcomponent = component.find(optionTypes[type]);
+      renderedSubcomponent = subcomponent.dive();
+    });
+
+    /* common tests */
+    it(`renders ${optionTypes[type]}`, () => {
+      expect(subcomponent).toBeTruthy();
+      expect(subcomponent.length).toBe(1);
+    });
+
+    /* type-specific tests */
+    switch (type) {
+      case 'dropdown': {
+        /* tests for dropdown */
+        it('contains select and options', () => {
+          const select = renderedSubcomponent.find('select'); // sprawdzamy obecność selecta
+          expect(select.length).toBe(1);
+
+          const emptyOption = select.find('option[value=""]').length; // sprawdzamy opcję z pustą wartością
+          expect(emptyOption).toBe(1);
+
+          const options = select.find('option').not('[value=""]'); // sprawdzamy mockowane wartości
+          expect(options.length).toBe(mockProps.values.length);
+          expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+          expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+        });
+
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find('select').simulate('change', {currentTarget: {value: testValue}});
+          expect(mockSetOrderOption).toBeCalledTimes(1); // sprawdzamy czy funkcja wykonała się dokładnie jeden raz
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue }); //sprawdzamy czy została wywołana z poprawnymi argumentami
+        });
+
+        break;
+      }
+    }
+  });
+}
