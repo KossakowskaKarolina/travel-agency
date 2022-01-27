71ce5edcbb5be3f047fe51302daeb2d2b8f467c1
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-01-27 20:51:35 +0100

    Add test for showing correct number of seconds in HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,17 +2,17 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  promoDescription: '.promoDescription',
+};
+
+const mockProps = {
+  title: 'Lorem ipsum',
+  promoDescription: '1234',
+};
+
 describe('Component HappyHourAd', () => {
-
-  const select = {
-    title: '.title',
-    promoDescription: '.promoDescription',
-  };
-
-  const mockProps = {
-    title: 'Lorem ipsum',
-    promoDescription: 'This is what our promo is about',
-  };
 
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
@@ -30,4 +30,37 @@ describe('Component HappyHourAd', () => {
     expect(component.find(select.title).text()).toEqual(mockProps.title);
     console.log(component.debug());
   });
+});
+
+const trueDate = Date;
+const mockDate = customDate => class extends Date { // zmieniamy działanie new Date() oraz Date.now(), aby zawsze zwracały tę samą wartość ustawioną przez nas
+  constructor(...args) { // powyżej definiujemy anonimową klasę (będzie istnieć tylko w tym bloku describe)
+    if(args.length){ // jeśli podamy argumenty, wywołamy konstruktor Date (czyli super) z tymi argumentami
+      super(...args); // odwołujemy się do konstruktora klasy rodzica tj. Date
+    } else { // jeśli nie podanmy argumentów wywołamy Date z podaną przez nas datą
+      super(customDate);
+    }
+    return this;
+  }
+  static now(){ // metoda statyczna, tj wywołaywana na klasie (a nie na instacji), czyli jako mockDate.now()
+    return (new Date(customDate)).getTime(); // getTime wykorzystane do przypisania daty i czasu do innego obiektu Date
+  }
+};
+
+const checkDescriptionAtTime = (time, expectedDescription) => {
+  it(`should show correct at ${time}`, () => {
+    global.Date = mockDate(`2019-05-14T${time}.135Z`); // korzystamy z obiektu gloal i podmiamy klasę Date
+
+    const component = shallow(<HappyHourAd {...mockProps} />);
+    const renderedTime = component.find(select.promoDescription).text();
+    expect(renderedTime).toEqual(expectedDescription);
+
+    global.Date = trueDate; // podmiana wyżej jest na stałe, więc po wykonaniu testu musimy ją odwrócić
+  });
+};
+
+describe('Component HappyHourAd with mocked Date', () => {
+  checkDescriptionAtTime('11:57:58', '122');
+  checkDescriptionAtTime('11:59:59', '1');
+  checkDescriptionAtTime('13:00:00', 23 * 60 * 60 + '');
 });
