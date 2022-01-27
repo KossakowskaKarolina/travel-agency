2a925f20ebc0c966f5f1f9b392cc6346b79d5a46
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-01-27 21:17:30 +0100

    Add test for countdown in HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
 
+  constructor(){
+    super();
+
+    /* run  every second */
+    setInterval(() => this.forceUpdate(), 1000);
+  }
+
   getCountdownTime(){
     const currentTime = new Date();
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
