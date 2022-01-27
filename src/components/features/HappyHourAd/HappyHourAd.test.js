ca0fcdd0b30595d63f42b10d6f8d8b2d89bd25f8
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-01-27 21:43:59 +0100

    Add test for rendering description after time delay in HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -97,3 +97,7 @@ describe('Component HappyHourAd with mocked Date', () => {
   checkDescriptionAtTime('12:00:00', mockProps.promoDescription);
   checkDescriptionAtTime('12:59:59', mockProps.promoDescription);
 });
+
+describe('Component HappyHourAd with mocked Date and delay', () => {
+  checkDescriptionAfterTime('11:59:58', 4, mockProps.promoDescription);
+});
