48e91b37d85b33db2f98c69aceaf9391289e649b
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-01-28 10:29:58 +0100

    Add formatTime funcionality

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,7 +1,11 @@
-export const formatTime = (seconds) => {
-  if(!seconds || typeof(seconds)!=='number' || seconds <= 0){
+export const formatTime = (time) => {
+  if(!time || typeof(time)!=='number' || time <= 0){
     return null;
+  } else {
+    const seconds = String(Math.floor(time%60)).padStart(2, '0');
+    const minutes = String(Math.floor((time/60)%60)).padStart(2, '0');
+    const hours = String(Math.floor(time/3600)).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
   }
-
-
 };
