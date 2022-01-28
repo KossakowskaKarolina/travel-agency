f5a5170148457eb710878a475d9a3eb5f2ce8955
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-01-28 14:43:35 +0100

    Add test for rendering correct time left in DaysToSummer

--- src/components/features/DaysToSummer/DaysToSummer.js
+++ src/components/features/DaysToSummer/DaysToSummer.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DaysToSummer = (props) => (
-  <div>
-    <h3 className='title'>{props.text}</h3>
-  </div>
-);
+class DaysToSummer extends React.Component {
+  getCountdownTime(){
+    const currentTime = new Date();
+    const currentDate = new Date(Date.UTC(currentTime.getUTCFullYear(),  currentTime.getUTCMonth(), currentTime.getUTCDate()));
+    let nextSummer = new Date(Date.UTC(currentTime.getUTCFullYear(), 5, 21));
+    let days = Math.floor((nextSummer.getTime() - currentDate.getTime())/(1000*60*60*24));
+
+    if(days <= 171 && days > 0){
+      return days;
+    } else if (days <= 0 && days >= -94){
+      return '';
+    } else if (days < -94 ) {
+      let nextSummer = new Date(Date.UTC(currentTime.getUTCFullYear()+1, 5, 21));
+      return Math.floor((nextSummer.getTime() - currentDate.getTime())/(1000*60*60*24));
+    }
+  }
+
+  render(){
+    //const countdownTime = this.getCountdownTime();
+    return (
+      <div>
+        <h3 className='title'>{this.props.text}</h3>
+        <h3 className='days'>{this.getCountdownTime()}</h3>
+      </div>
+    );
+  }
+
+}
+
 
 DaysToSummer.propTypes = {
   text: PropTypes.string,
--- src/components/features/DaysToSummer/DaysToSummer.test.js
+++ src/components/features/DaysToSummer/DaysToSummer.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import DaysToSummer from './DaysToSummer';
 
+const select = {
+  title: '.title',
+  days: '.days',
+};
+
 const mockProps = {
-  text: '44 days left',
+  text: 'days left',
+  days: '44',
 };
 
 describe('Component DaysToSummer', () => {
@@ -14,6 +20,40 @@ describe('Component DaysToSummer', () => {
 
   it('should render correct text', () => {
     const component = shallow(<DaysToSummer {...mockProps} />);
-    expect(component.find('.title').text()).toEqual(mockProps.text);
+    expect(component.find(select.title).text()).toEqual(mockProps.text);
   });
+});
+
+const trueDate = Date;
+const mockDate = customDate => class extends Date {
+  constructor(...args) {
+    if(args.length){
+      super(...args);
+    } else {
+      super(customDate);
+    }
+    return this;
+  }
+  static now(){
+    return (new Date(customDate)).getTime();
+  }
+};
+
+const checkDescriptionAtTime = (time, expectedDescription) => {
+  it(`should show correct at ${time}`, () => {
+    global.Date = mockDate(`${time}T10:34:04.543Z`);
+
+    const component = shallow(<DaysToSummer {...mockProps} />);
+    const renderedTime = component.find(select.days).text();
+    expect(renderedTime).toEqual(expectedDescription);
+
+    global.Date = trueDate;
+  });
+};
+
+describe('Component HappyHourAd with mocked Date', () => {
+  checkDescriptionAtTime('2022-01-28', '144');
+  checkDescriptionAtTime('2022-07-14', '');
+  checkDescriptionAtTime('2022-06-20', '1');
+  checkDescriptionAtTime('2022-10-10', '254');
 });
